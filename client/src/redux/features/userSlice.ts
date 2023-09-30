48f07a372cbf0f9ee9ab2c6b86fe5b64c3f567af
AlanAriasV <alan.arias.vargas@icloud.com>
2023-09-29 23:54:55 -0300

    level-selector-class

--- client/src/redux/features/userSlice.ts
+++ client/src/redux/features/userSlice.ts
@@ -1,20 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    rut: '',
-    name: 'User',
-    role: 'SUPERUSER'
-}
+  rut: "",
+  name: "User",
+  role: "SUPERUSER",
+};
 
 export const userSlice = createSlice({
-    name: 'user',
-    initialState,
-    reducers: {
-        swaptype: (state) => {
-            state.role = (state.role == 'SUPERUSER' ? 'STUDENT' : 'SUPERUSER')
-        }
-    }
-})
+  name: "user",
+  initialState,
+  reducers: {
+    swaptype: (state) => {
+      state.role = state.role == "SUPERUSER" ? "STUDENT" : "SUPERUSER";
+    },
+  },
+});
 
-export default userSlice.reducer
-export const { swaptype } = userSlice.actions
+export default userSlice.reducer;
+export const { swaptype } = userSlice.actions;
--- client/src/utils/types.ts
+++ client/src/utils/types.ts
@@ -1,34 +1,34 @@
 export type Level = {
-    id: string,
-    name: string
+  id: string;
+  name: string;
 };
 
 export type Student = {
-    run: number | string;
-    dv?: string;
-    name: string;
-    first_surname: string;
-    second_surname: string;
-    level: string;
+  run: number | string;
+  dv?: string;
+  name: string;
+  first_surname: string;
+  second_surname: string;
+  level: string;
 };
 
 export type Grade = {
-    id: number,
-    name: string,
-    date: Date,
-    classGroupId: number,
-    Results: {
-        examId: number,
-        studentId: number,
-        grade: number
-    }[],
-    classgroup: {
-        teacherId: number,
-        group: { letter: string, topic: string }
-    }
-}
+  id: number;
+  name: string;
+  date: Date;
+  classGroupId: number;
+  Results: {
+    examId: number;
+    studentId: number;
+    grade: number;
+  }[];
+  classgroup: {
+    teacherId: number;
+    group: { letter: string; topic: string };
+  };
+};
 
 export type User = {
-    rut: number
-    name: string
-}
+  rut: number;
+  name: string;
+};
