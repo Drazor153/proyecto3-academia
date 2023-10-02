03ad32c3e5d14d8a0310dd47f6d63b4fc0939ff2
Drazor153 <fabianjusto2341@gmail.com>
2023-10-02 06:15:10 -0300

    feat: teacher ctrl and route modified

--- server/src/routes/teachers.routes.ts
+++ server/src/routes/teachers.routes.ts
@@ -5,8 +5,10 @@ import { postGradeValidator } from '../utils/validators';
 const router = Router();
 
 router.get('/levels/:run', teachersControllers.getTeacherLevels);
+router.get('/grades/:year/:semester/:level', teachersControllers.getLevelQuizzes)
 
-// Route for put a grade to a student
-router.post('/grades', postGradeValidator, teachersControllers.postGrade);
+router.get('/grades/quizzes/:quizId', teachersControllers.getQuizGrades)
+router.post('/grades/quizzes', postGradeValidator, teachersControllers.postQuizzesGrades);
+
 
 export default router;
