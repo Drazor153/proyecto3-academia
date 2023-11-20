9fcec93f729acd2bd4b8a651e4fc4e09b8fd1094
Alan Arias Vargas <alan.arias.vargas@icloud.com>
2023-11-20 20:57:15 -0300

    Update language swap functionality and fix styling

--- client/src/components/LanguageSwap.tsx
+++ client/src/components/LanguageSwap.tsx
@@ -1,23 +1,29 @@
-import { useTranslation } from "react-i18next";
-import enFlag from '../assets/en.png';
+import { useTranslation } from 'react-i18next';
+import ukFlag from '../assets/uk.jpg';
 import esFlag from '../assets/es.png';
 
 function LanguageSwap() {
-    const { i18n } = useTranslation();
-    const languageFlag = i18n.language == 'es' ? esFlag : enFlag
-    const changeLanguageHandler = () => {
-        const languageValue = i18n.language == 'es' ? 'en' : 'es'
+  const { i18n } = useTranslation();
+  const languageFlag = i18n.language == 'es' ? esFlag : ukFlag;
+  const changeLanguageHandler = () => {
+    const languageValue = i18n.language == 'es' ? 'en' : 'es';
 
-        i18n.changeLanguage(languageValue);
-        localStorage.setItem('language', languageValue);
-    }
+    i18n.changeLanguage(languageValue);
+    localStorage.setItem('language', languageValue);
+  };
 
-    return (
-        <button className="lang-swapper" onClick={changeLanguageHandler}>
-            <img src={languageFlag} alt="" />
-            <span>{i18n.language == 'es' ? 'Español' : 'English'}</span>
-        </button>
-    )
+  return (
+    <button
+      className="lang-swapper"
+      onClick={changeLanguageHandler}
+    >
+      <img
+        src={languageFlag}
+        alt=""
+      />
+      <span>{i18n.language == 'es' ? 'Español' : 'English'}</span>
+    </button>
+  );
 }
 
-export default LanguageSwap
+export default LanguageSwap;
--- client/src/i18n.ts
+++ client/src/i18n.ts
@@ -1,8 +1,8 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 
-import translationEN from "./translation/en/translation.json";
-import translationES from "./translation/es/translation.json";
+import translationEN from './translation/en/translation.json';
+import translationES from './translation/es/translation.json';
 
 const resources = {
   en: {
@@ -17,7 +17,7 @@ const resources = {
 
 i18n.use(initReactI18next).init({
   resources,
-  lng: localStorage.getItem("language") ?? "es", //default language
+  lng: localStorage.getItem('language') ?? 'en', //default language
   keySeparator: false,
   interpolation: {
     escapeValue: false,
