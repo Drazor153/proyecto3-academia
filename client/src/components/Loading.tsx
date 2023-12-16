83d913e1f6fcc144a513c4c73ca12f66ab2aff9e
AlanAriasV <alan.arias.vargas@icloud.com>
2023-12-16 05:27:31 -0300

    loading page
    Co-authored-by: Camilo Valenzuela <mhylos@users.noreply.github.com>

---
+++ client/src/components/Loading.tsx
@@ -0,0 +1,16 @@
+export default function Loading() {
+	return (
+		<img
+			src='images/loading.png'
+			alt='loading-logo'
+			width={200}
+			height={200}
+			style={{
+				position: 'absolute',
+				top: '50%',
+				left: '50%',
+				transform: 'translate(-50%, -50%)',
+			}}
+		/>
+	);
+}
--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -4,61 +4,57 @@ import { useEffect } from 'react';
 import { ResponseMsg } from '../utils/types';
 import { useAutoLoginQuery } from '../redux/services/userApi';
 import { setUser } from '../redux/features/userSlice';
+import Loading from './Loading';
 
 interface ProtectedRouteProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 export function ProtectedRoute({ children }: ProtectedRouteProps) {
-  const user = useAppSelector(state => state.userReducer);
-  const dispatch = useAppDispatch();
+	const user = useAppSelector(state => state.userReducer);
+	const dispatch = useAppDispatch();
 
-  const location = useLocation();
+	const location = useLocation();
 
-  const { data: result, isLoading, isSuccess } = useAutoLoginQuery(null);
+	const { data: result, isLoading, isSuccess } = useAutoLoginQuery(null);
 
-  useEffect(() => {
-    if (user.run !== -1) {
-      fetch(`${import.meta.env.VITE_SERVER_HOST}/api/auth/refresh`, {
-        credentials: 'include',
-      })
-        .then(res => res.json())
-        .then(({ msg }: ResponseMsg) => {
-          console.log(msg);
-        })
-        .catch(() => {
-          console.log('Failed to fetch "refresh token"');
-        });
-    }
-  }, [user.run, location]);
+	useEffect(() => {
+		if (user.run !== -1) {
+			fetch(`${import.meta.env.VITE_SERVER_HOST}/api/auth/refresh`, {
+				credentials: 'include',
+			})
+				.then(res => res.json())
+				.then(({ msg }: ResponseMsg) => {
+					console.log(msg);
+				})
+				.catch(() => {
+					console.log('Failed to fetch "refresh token"');
+				});
+		}
+	}, [user.run, location]);
 
-  useEffect(() => {
-    if (isSuccess) {
-      if (user.run === -1) {
-        console.log('ProtectedRoute');
-        dispatch(setUser(result.userData));
-      }
-      console.log(user);
-    }
-  }, [result?.userData, dispatch, isSuccess, user]);
+	useEffect(() => {
+		if (isSuccess) {
+			if (user.run === -1) {
+				console.log('ProtectedRoute');
+				dispatch(setUser(result.userData));
+			}
+			console.log(user);
+		}
+	}, [result?.userData, dispatch, isSuccess, user]);
 
-  if (isLoading) {
-    console.log('isLoading');
-    return (
-      <>
-        <h1>Loading...</h1>
-      </>
-    );
-  }
+	if (isLoading) {
+		return <Loading />;
+	}
 
-  if (isSuccess || user.run !== -1) {
-    return (
-      <>
-        {children}
-        <Outlet />
-      </>
-    );
-  } else {
-    return <Navigate to={'/login'} />;
-  }
+	if (isSuccess || user.run !== -1) {
+		return (
+			<>
+				{children}
+				<Outlet />
+			</>
+		);
+	} else {
+		return <Navigate to={'/login'} />;
+	}
 }
